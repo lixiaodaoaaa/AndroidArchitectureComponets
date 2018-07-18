99953c6a569b50466e6232ac5156db5ab59e09b8
lixiaodaoaaa <lixiaodaoaaa@126.com>
2018-07-18 09:27:43 +0800

    git add livedata study

--- app/src/main/java/com/ihognqiqu/aac/livedata/LiveDataActivity.java
+++ app/src/main/java/com/ihognqiqu/aac/livedata/LiveDataActivity.java
@@ -7,45 +7,48 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
 import com.ihognqiqu.aac.R;
 
-public class LiveDataActivity extends AppCompatActivity {
-
-    private MutableLiveData<String> username;
-
+public class LiveDataActivity extends AppCompatActivity{
+    
+    
     private TextView tvUsername;
-
+    private MutableLiveData<String> liveDataUsernameStr;
+    
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_data);
-
-        tvUsername = findViewById(R.id.tv_username);
-
-        username = new MutableLiveData<>();
-
-        username.observe(this, new Observer<String>() {
+        tvUsername=findViewById(R.id.tv_username);
+        initDatas();
+    }
+    
+    private void initDatas(){
+        liveDataUsernameStr=new MutableLiveData<>();
+        liveDataUsernameStr.observe(this, new Observer<String>(){
             @Override
-            public void onChanged(@Nullable String s) {
+            public void onChanged(@Nullable String s){
                 tvUsername.setText(s);
             }
         });
     }
-
-    public void onClickMainThread(View view) {
-        username.setValue("New User Name by main thread");
+    
+    public void onClickMainThread(View view){
+        liveDataUsernameStr.setValue("lixiaodaaaaMain");
     }
-
-    public void onClickThread(View view) {
-        new Thread(new Runnable() {
+    
+    public void onClickThread(View view){
+        new Thread(new Runnable(){
             @Override
-            public void run() {
+            public void run(){
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                username.postValue("New User Name by background thread");
+                liveDataUsernameStr.postValue("lixxiaodaaaaThread");
             }
         }).start();
     }
